66d6545e5f913841912ae72d93f39a26647f1741
Marcelo Aliquintuy <mmaliq@gmail.com>
2013-09-17 18:02:28 -0300

    Cambio de puerto de mongodb

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,7 +1,28 @@
 grails {
     mongo {
         host = "localhost"
-        port = 27107
+        port = 27017
         databaseName = "ml2"
+    }
+}
+
+environments {
+    development {
+        mongo {
+            dbCreate = "create-drop"
+            databaseName ="dev"
+        }
+    }
+    test {
+        mongo {
+            dbCreate = "update"
+            databaseName ="test"
+        }
+    }
+    production {
+        mongo {
+            dbCreate = "update"
+            databaseName ="prod"
+        }
     }
 }
