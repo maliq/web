1346eccfc068b8ae153eef633fe36e431ab9f364
Marcelo Aliquintuy <mmaliq@gmail.com>
2013-09-18 00:38:56 -0300

    program funcionando

---
+++ grails-app/domain/web/Program.groovy
@@ -0,0 +1,58 @@
+package web
+
+import java.util.List
+import org.bson.types.ObjectId
+
+class Program {
+	ObjectId id
+  	String name
+  	String description
+  	String episode
+  	String web
+  	String year
+  	String imdbSelected
+  	String wikipediaSelected
+
+  	static hasMany = [imdbResults: ImdbResult, wikipediaResults: WikipediaResult]
+
+    static constraints = {
+    	imdbSelected nullable: true
+  		wikipediaSelected nullable: true
+    }
+
+    static mapping = {
+        collection "programs"
+    }
+
+    static embedded = ['imdbResults','wikipediaResults']
+}
+
+class ImdbResult{
+	String url
+  	String imdbId
+  	String title
+  	String postTitle
+
+  	static mapping = {
+        version false
+        autoTimestamp false
+    }
+
+    @Override
+    public String toString(){
+        return title
+    }
+}
+
+class WikipediaResult{
+	String title
+	static mapping = {
+        version false
+        autoTimestamp false
+    }
+
+    @Override
+    public String toString(){
+        return title
+    }
+}
