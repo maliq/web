4379e5b72b6f635a7544aeffafa23246b5f081cb
Marcelo Aliquintuy <mmaliq@gmail.com>
2013-10-22 10:51:38 -0300

    se cambia el cod a atributo y se deja _id como autogenerado

--- grails-app/domain/web/Program.groovy
+++ grails-app/domain/web/Program.groovy
@@ -4,44 +4,45 @@ import java.util.List
 import org.bson.types.ObjectId
 
 class Program {
-	ObjectId id
-  	String name
-  	String description
-  	String episode
-  	String web
-  	String year
-  	String imdbSelected
-  	String wikipediaSelected
+  ObjectId id
+  Integer cod
+	String name
+	String description
+	String episode
+	String web
+	String year
+	String imdbSelected
+	String wikipediaSelected
 
-  	static hasMany = [imdbResults: ImdbResult, wikipediaResults: WikipediaResult]
+	static hasMany = [imdbResults: ImdbResult, wikipediaResults: WikipediaResult]
 
-    static constraints = {
-    	imdbSelected nullable: true
-  		wikipediaSelected nullable: true
-    }
+  static constraints = {
+  	imdbSelected nullable: true
+		wikipediaSelected nullable: true
+  }
 
-    static mapping = {
-        collection "programs"
-    }
+  static mapping = {
+      collection "programs"
+  }
 
-    static embedded = ['imdbResults','wikipediaResults']
+  static embedded = ['imdbResults','wikipediaResults']
 }
 
 class ImdbResult{
 	String url
-  	String imdbId
-  	String title
-  	String postTitle
+	String imdbId
+	String title
+	String postTitle
 
-  	static mapping = {
-        version false
-        autoTimestamp false
-    }
+	static mapping = {
+      version false
+      autoTimestamp false
+  }
 
-    @Override
-    public String toString(){
-        return title
-    }
+  @Override
+  public String toString(){
+      return title
+  }
 }
 
 class WikipediaResult{
